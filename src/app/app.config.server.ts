d1c46d79040ef1258ffb2f0a0b5d2beef0f96ed0
Alexis Pelé <alexis@agence-polux.fr>
2025-09-16 15:13:33 +0200

    Default theme colours

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -3,10 +3,22 @@ import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 
+import { providePrimeNG } from 'primeng/config';
+import { Synetics } from './theme';
+
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering(withRoutes(serverRoutes))
-  ]
+    providers: [
+        provideServerRendering(withRoutes(serverRoutes)),
+        providePrimeNG({
+            ripple: true,
+            theme: {
+                preset: Synetics,
+                options: {
+                    darkModeSelector: false,
+                }
+            }
+        })
+    ]
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
---
+++ src/app/theme.ts
@@ -0,0 +1,55 @@
+//mypreset.ts
+import { definePreset } from '@primeuix/themes';
+import Aura from '@primeuix/themes/aura';
+
+export const Synetics = definePreset(Aura, {
+    semantic: {
+        'cyan': {
+            DEFAULT: '#004F71',
+            50: '#F1FBFF',
+            100: '#D7F3FF',
+            200: '#A4E4FF',
+            300: '#71D4FF',
+            400: '#3EC5FF',
+            500: '#0BB6FF',
+            600: '#0096D7',
+            700: '#0073A4',
+            800: '#004F71',
+            900: '#002B3E',
+            950: '#001A25'
+        },
+        primary: {
+            50: '{cyan.50}',
+            100: '{cyan.100}',
+            200: '{cyan.200}',
+            300: '{cyan.300}',
+            400: '{cyan.400}',
+            500: '{cyan.500}',
+            600: '{cyan.600}',
+            700: '{cyan.700}',
+            800: '{cyan.800}',
+            900: '{cyan.900}',
+            950: '{cyan.950}'
+        },
+    },
+    components: {
+        button: {
+            root: {
+                borderRadius: '1rem 0 1rem 0'
+            },
+            colorScheme: {
+                light: {
+                    root: {
+                        primary: {
+                            background: '{primary.800}',
+                            borderColor: '{primary.800}',
+                            focusRing: {
+                                color: '{primary.800}'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+});
