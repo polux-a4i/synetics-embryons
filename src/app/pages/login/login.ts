d7b4a40db4c4339873c6ae2c53519edf096c2788
Alexis Pelé <alexis@agence-polux.fr>
2025-09-17 09:43:15 +0200

    Added font body family

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabel } from 'primeng/floatlabel';
 
 @Component({
   selector: 'syn-login',
-  imports: [ButtonModule, CardModule],
+  imports: [ButtonModule, CardModule, InputTextModule, FloatLabel],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
