88b439d49abbb18aa6c5ce4b6e189080d5f92e4d
Alexis Pelé <alexis@agence-polux.fr>
2025-09-17 17:01:40 +0200

    Updated build assets

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 447, hash: '3c9d5bf9ffe0ca5a27982458cdeaaf4610ec4118e1978e9f8d0de56d387a93bd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 960, hash: 'a9ce2690d950d8937f9940c26d7589e68a6916a239171c15af28c71914d584b8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 21574, hash: '79a2e5101adb2068179f3a3157d334ec33d48ee2fb8842756be6c6b309a45978', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 2341, hash: '5b13fc1bb9e626dd508b87b012ace028ecae8b1115e670abc5f8e8237630392e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 960, hash: 'd07d4ca907a76c97512229e2aaf24a4033c4c42b0e7be5a03cec50ed432a5863', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 66893, hash: '81d1d35a4de375fab6a64b1e94bfce246e83b26c2f55a603c64379209c5e6f3c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-FDESBVFQ.css': {size: 27403, hash: '28dlA/i4BCs', text: () => import('./assets-chunks/styles-FDESBVFQ_css.mjs').then(m => m.default)}
   },
 };
