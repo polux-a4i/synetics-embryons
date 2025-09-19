a70a66b331ed89d463c682834ce3ec0fe6879e72
Alexis Pelé <alexis@agence-polux.fr>
2025-09-19 16:25:07 +0200

    Built for prod

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -8,13 +8,18 @@ export default {
   {
     "renderMode": 2,
     "route": "/synetics-embryons"
+  },
+  {
+    "renderMode": 2,
+    "route": "/synetics-embryons/collections"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 2341, hash: '5b13fc1bb9e626dd508b87b012ace028ecae8b1115e670abc5f8e8237630392e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 960, hash: 'd07d4ca907a76c97512229e2aaf24a4033c4c42b0e7be5a03cec50ed432a5863', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 66893, hash: '81d1d35a4de375fab6a64b1e94bfce246e83b26c2f55a603c64379209c5e6f3c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-FDESBVFQ.css': {size: 27403, hash: '28dlA/i4BCs', text: () => import('./assets-chunks/styles-FDESBVFQ_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 2341, hash: '8078d2cdd4ac4846110cbaeec125a8651e2a6abb6df8a73d117b598c833a21e7', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 960, hash: 'eb1fbe96b44bbfcec7b92d8de5d61cb0bf9230fdc727d0c7ac8239c09389aeef', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 68015, hash: '19067c560cff93bd3441c14db4ac7cc9b335353d637507461d4d551afe62f9f4', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'collections/index.html': {size: 113181, hash: 'a23457381936ccf72500acc118531715b73acc529cb4bb043f2dcfd0432dfc53', text: () => import('./assets-chunks/collections_index_html.mjs').then(m => m.default)},
+    'styles-JCIW5VQW.css': {size: 29864, hash: 'R0m+ABodxIs', text: () => import('./assets-chunks/styles-JCIW5VQW_css.mjs').then(m => m.default)}
   },
 };
