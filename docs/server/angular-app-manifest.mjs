92392376b63d3f4c6f831a127c3a6b1130a14cf5
Alexis Pelé <alexis@agence-polux.fr>
2025-09-22 10:55:34 +0200

    Fixed broken urls for fonts

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -16,10 +16,10 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 2341, hash: 'e993149bac9739dc91880aa995768be7addc6bcd6a702bce411e71a385921c89', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 960, hash: 'd2d121d92e70f2c0be4f2fb36c18b46f469691ecb7b1810c4ec09fbfba5e9817', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 68005, hash: '1a1f3dc8bfc13f5c7aa81ab43da3a2cd87b9f576c4e3036448d2a62932de6443', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'collections/index.html': {size: 143856, hash: '1e2d2b768e663931e7fcd47b3e229f15571dc68ad3e42ad24c412079ac58df97', text: () => import('./assets-chunks/collections_index_html.mjs').then(m => m.default)},
-    'styles-NX34CPG5.css': {size: 30046, hash: '7Nmemzfq5Jk', text: () => import('./assets-chunks/styles-NX34CPG5_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 2317, hash: '816d4cd62a780c0ec16913af4f8b24e9e1a780cea48d60438ab768727613a83f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 960, hash: 'b03224a25139d270b340c26145fac1ff71d15aa0bedab9a69c7d4935fbbdda9c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 67981, hash: '8048f0472c60fd484b7c8883fa7590c1c37085a7daffaa52d8e597ba2db93b05', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'collections/index.html': {size: 143848, hash: 'e8d97fa8ab25c05c7f8b4e1c93f7064c3bd2613789f5d6ae390cae7339658b1f', text: () => import('./assets-chunks/collections_index_html.mjs').then(m => m.default)},
+    'styles-MHSQW4DF.css': {size: 30038, hash: '2KRDl9H/jxA', text: () => import('./assets-chunks/styles-MHSQW4DF_css.mjs').then(m => m.default)}
   },
 };
