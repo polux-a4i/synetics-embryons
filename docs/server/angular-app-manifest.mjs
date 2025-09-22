fa5a6a31c392efaa24287be42cc51bb6d65bb205
Alexis Pelé <alexis@agence-polux.fr>
2025-09-22 14:47:39 +0200

    Updated favicon

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -16,10 +16,10 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 2317, hash: 'e901792ced3aefee9fa05b275fd0b8bb01fbc1d951216df36995527a5fe424ab', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 960, hash: '8b9eaf4cea25838215268d7911f47def9a90c6fb0b08f3146b96eaa6a1914641', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 69386, hash: 'e39d0655504e07c8b5993d11a812384420ac444959d3b51d04a7573142d73583', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'collections/index.html': {size: 147532, hash: '52636e85268772ebdf3cdc2772f3a8cb8bddbbda55e72a0ca2cef050588098ae', text: () => import('./assets-chunks/collections_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2687, hash: 'c501fe5bca5f7fd498c24ae8dfe85aa45e4335430a65e7408cc9be8af70e6332', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1336, hash: '82f06905ba30380d87179610bbb1fff1940d51fde237e9c2cc08eadd1ae2034d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 69755, hash: '43d412d4b282bc8cf586091c84b5445e35e1aeb98c9b2ea8e33c5a1a534679fe', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'collections/index.html': {size: 147901, hash: '52bcdf550a050c0b4bbc7af3f727c22c6ffcad7df0f8931e1e237c354acefc18', text: () => import('./assets-chunks/collections_index_html.mjs').then(m => m.default)},
     'styles-MHSQW4DF.css': {size: 30038, hash: '2KRDl9H/jxA', text: () => import('./assets-chunks/styles-MHSQW4DF_css.mjs').then(m => m.default)}
   },
 };
