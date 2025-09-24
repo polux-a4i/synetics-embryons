663a92edf0f3aaca5eec3d96dd554ee989b7b2d0
Alexis Pelé <alexis@agence-polux.fr>
2025-09-24 11:55:22 +0200

    Finished step 6

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -16,10 +16,10 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 2775, hash: '024aea6a7d7877a934a65cf93db23b087bda15738ae688431b8ffd46d9d2133c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1336, hash: 'a980732747bad090ba59f21cc69d810b382d0eb6f6ddc9c3ce450878be54f070', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 69830, hash: '3610116f1384147875e625afbc1cbffefbaba1a6bcdbfd0c80a194e1f584e1c7', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'collections/index.html': {size: 172483, hash: '354d5375d10080bfda13ab6bcf5f12c963c4f45cc8e3f20f934cc5bccc2cb573', text: () => import('./assets-chunks/collections_index_html.mjs').then(m => m.default)},
-    'styles-NRQ6RTXQ.css': {size: 30397, hash: 'PDo7cRFr7KY', text: () => import('./assets-chunks/styles-NRQ6RTXQ_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 2775, hash: '942584275543599219b5898ae11ee613033a17d55b8fa2cd2139be5d0e917b36', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1336, hash: '9641553f5ebb61f57c9a0ee2aefcdae961a514f77b99eda28a15cfcb68911e32', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 69830, hash: 'ce4cdfd0a256b5298cf52a7c87b91592f7a0b66fff6bad86eac760ba0cab29ad', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'collections/index.html': {size: 173143, hash: 'c3659cf85169b3c5d08777036e48cc4f5d931aed6c12afa6b5396b0c1bc089c1', text: () => import('./assets-chunks/collections_index_html.mjs').then(m => m.default)},
+    'styles-5HTCTY3M.css': {size: 30444, hash: 'tH2YitLUrqk', text: () => import('./assets-chunks/styles-5HTCTY3M_css.mjs').then(m => m.default)}
   },
 };
