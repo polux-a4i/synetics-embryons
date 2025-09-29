7687a283aa5ab9555f60ea6a7d1a7ccba6ae168e
Alexis Pelé <alexis@agence-polux.fr>
2025-09-29 14:10:29 +0200

    Added settings menu

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -16,10 +16,10 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 2776, hash: 'd01882c17e39bf17b1f4b883b13caec364fb40e8b096d1601fb0d0720e50a0a3', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1336, hash: '2ffd65b949c2e25a168e3d429b0ef1a5a1a5b14e9f277c1c141be873f971d97d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 69831, hash: 'b0330cfa961cdf0d2621bc03e3aba22b606577d80c51f52bac706dc895c379d6', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'collections/index.html': {size: 173406, hash: 'fb7c599e3156899538386d1d671ae3d1e69b5032af391691fe76554fa46672cf', text: () => import('./assets-chunks/collections_index_html.mjs').then(m => m.default)},
-    'styles-EUTZTMWV.css': {size: 30749, hash: 'FnEq8r8jV9s', text: () => import('./assets-chunks/styles-EUTZTMWV_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 2776, hash: '78c72bc27907a85b43c4c06ecf32e3ee69e3aa1d0518b2122aee68b8afc68327', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1336, hash: 'f98e34de3a10999c088e6fc0eaca2105ff7bd49229fb9e6ac6af1642f9fc7eca', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 69991, hash: 'd3a252613066d7fb4f32e12fce88079de5cc0249fd62970287715215e5d4a9bc', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'collections/index.html': {size: 175089, hash: '47f67aece826fd26517b13e61525e0e56711ff3cd88e894913c52a7dab45ecbd', text: () => import('./assets-chunks/collections_index_html.mjs').then(m => m.default)},
+    'styles-PBMBAHPW.css': {size: 30795, hash: 'Vmx61p+1Ahw', text: () => import('./assets-chunks/styles-PBMBAHPW_css.mjs').then(m => m.default)}
   },
 };
