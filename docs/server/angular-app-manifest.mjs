d3b811366bf64584454bad6f73e8c46c54e4e918
Alexis Pelé <alexis@agence-polux.fr>
2025-10-02 15:02:49 +0200

    Added icon to autocomplete fields

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -16,10 +16,10 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 2776, hash: 'd2841d625fe836a16e66bde7677c6157cbeaea0269aa5c8ebf88ed4692346145', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1336, hash: 'c2ba25efadf98145d412db0b2b4c0393f1ed65a27adfffe530f3eb0c4971c053', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 73499, hash: '55ccb80b5ca2b76dc02d182ec48e4234a3989092d97e3998e9578c5128d43f56', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'collections/index.html': {size: 181375, hash: '43a637901489fd5c02ec54f0b9dbef4b191fd62fc2aed7a0d102c47e897cd121', text: () => import('./assets-chunks/collections_index_html.mjs').then(m => m.default)},
-    'styles-EU777IVA.css': {size: 30889, hash: 'GM4pI3oB7DI', text: () => import('./assets-chunks/styles-EU777IVA_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 2792, hash: '5c5f716c861243e14f455dd5578a971e6858910df1b0dc27fe878e8c62bbd865', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1336, hash: 'de1077d8385a34e8f0ca0490b3001cb8309ce6fa6c15faab1722dbb0e33d3099', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 73554, hash: 'cf516c5d4ac07be310006ee7d8d115feaebaef7a9cfc932a992aa216250b80da', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'collections/index.html': {size: 181688, hash: '72f7d71242df20c82470cb0108bc0a0d305ebe1573d0eeca7dfe9c1d07f327d3', text: () => import('./assets-chunks/collections_index_html.mjs').then(m => m.default)},
+    'styles-S5XXHVJE.css': {size: 31217, hash: 'bHzgzL9CF2I', text: () => import('./assets-chunks/styles-S5XXHVJE_css.mjs').then(m => m.default)}
   },
 };
